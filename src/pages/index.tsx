d11a3d27352d1953700a609541028b6000ddafea
BlackstoneEngineering <austin@riscv.org>
2025-08-23 00:33:24 -0500

    fix getting started button link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/isa">
+            to="/docs/reference/isa/index.html">
             View the Specification
           </Link>
         </div>
